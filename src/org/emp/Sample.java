93676a201d9aeb2ddaf6ef3b8fcdf17949010b66
gowtham24008 <gowtham24008@gmail.com>
2022-01-16 18:36:52 +0530

    hi there

--- src/org/emp/Sample.java
+++ src/org/emp/Sample.java
@@ -10,6 +10,9 @@ System.out.println("emplyoee is 12334");
 		// TODO Auto-generated method stub
 System.out.println("employee is tamil");
 	}
+	   private void empdresscode() {
+		System.out.println("blue jeans,black t shirt");
+	}
 	}
 		
 
