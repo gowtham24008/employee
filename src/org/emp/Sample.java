efebd2c2f0326500d54da9820b4f88aaa7051bea
gowtham24008 <gowtham24008@gmail.com>
2022-01-16 17:57:46 +0530

    every one ok

---
+++ src/org/emp/Sample.java
@@ -0,0 +1,15 @@
+package org.emp;
+
+public class Sample {
+	  public void empid() {
+		// TODO Auto-generated method stub
+System.out.println("emplyoee is 12334");
+
+	}
+	   public void empname() {
+		// TODO Auto-generated method stub
+System.out.println("employee is tamil");
+	}
+	}
+		
+
